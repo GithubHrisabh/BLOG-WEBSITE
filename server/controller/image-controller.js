7564243764de1b697c6f1b40df3aa7212f8528cc
Hrisabh Jha <89374501+GithubHrisabh@users.noreply.github.com>
2021-10-21 21:57:31 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,39 +1,32 @@
+import grid from 'gridfs-stream';
+import mongoose from 'mongoose';
+
+const url = 'http://localhost:8000';
 
 
-import Comment from '../model/comment.js';
+let gfs;
+const conn = mongoose.connection;
+conn.once('open', () => {
+    gfs = grid(conn.db, mongoose.mongo);
+    gfs.collection('fs');
+});
 
 
-export const newComment = async (request, response) => {
-    try {
-        const comment = await new Comment(request.body);
-        comment.save();
+export const uploadImage = (request, response) => {
+    if(!request.file) 
+        return response.status(404).json("File not found");
+    
+    const imageUrl = `${url}/file/${request.file.filename}`;
 
-        response.status(200).json('Comment saved successfully');
-    } catch (error) {
-        response.status(500).json(error);
-    }
+    response.status(200).json(imageUrl);    
 }
 
-
-export const getComments = async (request, response) => {
-    try {
-        const comments = await Comment.find({ postId: request.params.id });
-        
-        response.status(200).json(comments);
+export const getImage = async (request, response) => {
+    try {   
+        const file = await gfs.files.findOne({ filename: request.params.filename });
+        const readStream = gfs.createReadStream(file.filename);
+        readStream.pipe(response);
     } catch (error) {
-        response.status(500).json(error)
-    }
-}
-
-export const deleteComment = async (request, response) => {
-    try {
-        console.log(request.params.id);
-        const comment = await Comment.findById(request.params.id);
-        console.log(comment);
-        await comment.delete()
-
-        response.status(200).json('comment deleted successfully');
-    } catch (error) {
-        response.status(500).json(error)
+        response.status(500).json('Failed to fetch image', error);
     }
 }
